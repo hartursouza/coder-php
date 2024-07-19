37c8d15c0002336fb2b74f2fb3b59ae149fbf506
Hartur Souza <hartursouza3@gmail.com>
2024-07-19 19:13:05 -0300

    Initial commit

---
+++ template/exercicio.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+        rel="stylesheet" />
+    <link rel="stylesheet" href="./assets/css/style.css" />
+    <link rel="stylesheet" href="./assets/css/exercicio.css">
+    <title>Document</title>
+</head>
+
+<body class="exercicio">
+    <header class="header">
+        <h1>Curso PHP</h1>
+        <h2>Índice dos Exercícios</h2>
+    </header>
+    <nav class="navigation">
+        <a href="<?= "./{$_GET['dir']}/{$_GET['file']}.php"?>" class="verde">Sem Formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
+    <main class="main">
+        <div class="content">
+            <?php include("./{$_GET['dir']}/{$_GET['file']}.php");
+            
+            ?>
+        </div>
+    </main>
+    <footer class=" footer">COD3R & ALUNOS &copy; <?= date("Y")?></footer>
+</body>
+
+</html>
---
+++ template/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+        rel="stylesheet" />
+    <link rel="stylesheet" href="./assets/css/style.css" />
+    <title>Document</title>
+</head>
+
+<body>
+    <header class="header">
+        <h1>Curso PHP</h1>
+        <h2>Índice dos Exercícios</h2>
+    </header>
+    <main class="main">
+        <div class="content">
+            <nav class="models">
+                <div class="model verde">
+                    <h3>Módulo 01</h3>
+                    <ul>
+                        <li><a href="/exercicio.php?dir=teste&file=teste">Exercício A</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </main>
+    <footer class="footer">COD3R & ALUNOS &copy; <?= date("Y")?></footer>
+</body>
+
+</html>
